0a492d99bad13770f941d9d2e200b242ad5d3614
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2022-03-28 18:50:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status="";
+objects=[];
 
 function setup(){
     canvas=createCanvas(480,380);
@@ -10,6 +11,7 @@ function setup(){
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML="status: detecting objects";
+    objectName=document.getElementById("input").value;
 }
 
 function modelLoaded(){
@@ -17,6 +19,39 @@ function modelLoaded(){
     status=true;
 }
 
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
 function draw(){
     image(video,0,0,480,380);
+    if(status!=""){
+        objectDetector.detect(video,gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status: Objects Detected";
+            fill("#fc6fbd");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"% ",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#fc6fbd");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label==objectName){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object").innerHTML=objectName+" Found";
+                synth=window.speechSynthesis;
+                utterThis=new SpeechSynthesisUtterance(objectName+"Found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("object").innerHTML=objectName+" Not Found";
+            }
+        }
+    }
 }
